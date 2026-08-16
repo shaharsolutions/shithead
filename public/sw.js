29231472d33703d3d113146bd15a7929f711fad2
shaharsolutions <shaharsolutions@gmail.com>
2026-08-16 14:09:36 +0300

    Add PWA install button to main screen

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'shithead-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+  '/apple-touch-icon.png',
+  '/lobby_hero.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
